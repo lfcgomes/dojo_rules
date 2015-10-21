056b172411d84bfe1e0dbfd4d2b7f25de32bed7e
Luís Gomes <lfcgomes@gmail.com>
2015-10-21 16:37:09 +0100

    script coiso

--- another_script.rb
+++ another_script.rb
@@ -1,1 +1,12 @@
- jnjkashkdjhaskjdhakjshdkjashdkjahsdkjahskjdhaskjhdaskj
+askjd asgasdjkgas dbakjdh ja sd.jkhasjd alskhd kajsdkagsdjhasd\a s
+dasd
+ assert(d as
+ 	d 
+ 	asd 
+ 	assert_difference " assert(d assert(d as
+ 		das 
+ 		assert_difference "  das assert(das d, "Failure message.").count", 1 do
+ 		  
+ 		end, "Failure message."), "Failure message.").count", 1 do
+ 	  
+ 	end, "Failure message.")
